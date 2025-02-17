1074ec814d5ffff66fdce7e4044a822424c60eca
Lenilson Santos <lenilsonrodrigo123@gmail.com>
2025-02-17 19:38:37 -0300

    put atualizar

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -89,6 +89,14 @@ namespace TrilhaApiDesafio.Controllers
 
             // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
             // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
+            tarefaBanco.Status = tarefa.Status;
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Titulo = tarefa.Titulo;
+
+            _context.Tarefas.Update(tarefaBanco);
+            _context.SaveChanges();
+
             return Ok();
         }
 
